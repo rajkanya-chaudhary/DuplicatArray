fefab574168e63b628879e690b597d143d56c7b4
rajkanya-chaudhary <dc5068786@gmail.com>
2024-02-10 23:05:53 +0530

    create a program if you given any String than check number which Stirng  is larger String

---
+++ LargesrString.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class LargesrString
+ {
+ public static void main(String arg[])
+  {
+  String fruits[] = {"apple","mango","banana"};
+  String largest = fruits[0];
+  for(int i = 1;i<fruits.length;i++)
+   {
+   if(largest.compareTo(fruits[i])< 0)
+   {
+    largest = fruits[i];
+   }
+ }
+System.out.println("largest fruits of " +largest);
+}
+}
+   
+  
